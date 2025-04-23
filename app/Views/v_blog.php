ae53a9881686da9582c21b119b7325518d9e27d6
Shafina <shafinaardelia0@gmail.com>
2025-04-23 10:33:46 +0800

    Initialize repository

---
+++ app/Views/v_blog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+</head>
+<body>
+    <h1>Welcome to My Blog!</h1>
+    <p>Hope you enjoy the journey here</p>
+</body>
+</html>
+
+<!-- controller dan model pake title case -->
+<!-- view make small case semua -->
+
+<!-- bikin controler baru -->
+ <!-- bikin routes nya -->
+  <!-- bikin viewnya -->
+
+
+<!-- cara install ci nya  -->
+ <!-- download ci, extract ke htdocs (xampp) atau www (laragon) -->
+  <!-- buka file php.ini (xampp cukup klik config, kalau laragon cari di file C:\laragon\bin\php\php-8.1.10-Win32-vs16-x64 cari php aja) -->
+   <!-- cari intl, lalu hapus titik koma untuk konfigurasi -->
+    <!-- simpan lalu close app -->
+     <!-- buka lagi tools nya -->
+      <!-- ganti nama folder ci yang di download (misal ci-web)-->
+       <!-- buka url berikut di browser (localhost/ci-web/public) -->
